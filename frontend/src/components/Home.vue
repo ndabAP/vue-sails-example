1d2a98bfb31318d14e5bd503173efb42139809ee
cetaju <kontakt@julian-claus.de>
2017-04-13 12:03:28 +0200

    extended welcome message

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -2,7 +2,8 @@
 <el-row :gutter="20">
   <el-col :span="10" :offset="6">
     <h1>Home</h1>
-    <p>Hey, you!</p>
+    <p>Welcome to the product paradise. First register, then login and add some products. Finally, buy the created
+    products.</p>
   </el-col>
 </el-row>
 </template>
