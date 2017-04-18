615e753e0242752001765600dd5ec877d5876672
cetaju <kontakt@julian-claus.de>
2017-04-18 19:22:10 +0200

    test if vuex action is called

--- frontend/test/unit/specs/Register.spec.js
+++ frontend/test/unit/specs/Register.spec.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import { mount } from 'avoriaz'
+import sinon from 'sinon'
 import VueResource from 'vue-resource'
 import BootstrapVue from 'bootstrap-vue'
 import store from './../../../state/index'
@@ -8,26 +11,43 @@ Vue.use(BootstrapVue)
 Vue.use(VueResource)
 
 describe('Register', () => {
-  it('should accept inputs', done => {
-    const Constructor = Vue.extend(Register)
-    const vm = new Constructor({
-      store,
-      components: {
-        Register
-      }
-    }).$mount()
+  it('should accept inputs', () => {
+    const wrapper = mount(Register, {store})
 
     let name = 'Hans'
     let password = '123'
 
-    vm.name = name
-    vm.password = password
+    const nameInput = wrapper.find('input')[0]
+    const passwordInput = wrapper.find('input')[1]
 
-    Vue.nextTick()
-      .then(() => {
-        expect(vm.$el.getElementsByTagName('input')[0].value).to.equal(name)
-        expect(vm.$el.getElementsByTagName('input')[1].value).to.equal(password)
-      })
-      .catch(done())
+    nameInput.element.value = name
+    passwordInput.element.value = password
+
+    nameInput.simulate('input')
+    passwordInput.simulate('input')
+
+    expect(wrapper.vm.$store.state.user.name).to.equal(name)
+    expect(wrapper.vm.$store.state.user.password).to.equal(password)
+  })
+
+  it('should call vuex action if button is clicked', () => {
+    const actions = {
+      saveUser: sinon.stub()
+    }
+    const store = new Vuex.Store({
+      state: {
+        user: {
+          name: 'Hans',
+          password: '123'
+        }
+      },
+      actions: actions
+    })
+
+    const wrapper = mount(Register, {store})
+    const button = wrapper.find('button')[0]
+    button.simulate('click')
+
+    expect(actions.saveUser.calledOnce).to.equal(true)
   })
 })
