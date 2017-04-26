a756d08adf67587b6bc4f920f02832b351f8e2e8
cetaju <kontakt@julian-claus.de>
2017-04-26 19:37:22 +0200

    built for production

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@
     <b-navbar toggleable type="inverse" variant="primary">
       <b-nav-toggle target="nav_collapse"></b-nav-toggle>
 
-      <b-link class="navbar-brand" to="#">
+      <b-link class="navbar-brand" :to="{ name: 'Home'}">
         <span>Product paradise</span>
       </b-link>
 
