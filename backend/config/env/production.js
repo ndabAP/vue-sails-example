81732e286a4ea3036a7c8fb6d10fc748ea268617
cetaju <kontakt@julian-claus.de>
2017-06-11 09:46:07 +0200

    newer heroku node version and start in production environment

--- backend/config/env/production.js
+++ backend/config/env/production.js
@@ -25,7 +25,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  // port: 80,
+  port: 80,
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
