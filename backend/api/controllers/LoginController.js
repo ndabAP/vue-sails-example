eb371748a4b755d8036cc52f45f7c277d9021f9b
David Kolosowski <davidk@syngro.local>
2017-07-14 11:39:53 +0100

    Use bcrypt for password hashing

--- backend/api/controllers/LoginController.js
+++ backend/api/controllers/LoginController.js
@@ -4,7 +4,10 @@ module.exports = {
    * @param req
    * @param res
    */
+  
   post: (req, res) => {
+    let standardErrorMessage = 'The username and password that you entered did not match our records. Please double-check and try again.';
+
     let {
       name,
       password
@@ -18,18 +21,30 @@ module.exports = {
       .exec((error, user) => {
         if (error) return res.serverError(error)
 
-        if (user) {
-          sails.log.info('User logged in', user)
+        if(!user) return res.json(401, {err: standardErrorMessage});
 
-          let encryptedId = CryptographyService.encrypt(user.id)
-          res.cookie('user', encryptedId)
+        User.validPassword(password, user, function(err, valid) {
+          if (err) {
+            return res.json(401, {err: standardErrorMessage});
+          }
 
-          return res.json({
-            token: TokenService.issue({
-              id: user.id
-            })
-          })
-        }
+          if (!valid) {
+            sails.log.info('Error', user)
+
+            return res.json(401, {err: standardErrorMessage});
+          } else {
+            sails.log.info('User logged in', user);
+
+            let encryptedId = CryptographyService.encrypt(user.id)
+            res.cookie('user', encryptedId)
+
+            return res.json({
+              token: TokenService.issue({
+                id: user.id
+              })
+            });
+          }
+        });
       })
   }
 }
--- backend/api/models/User.js
+++ backend/api/models/User.js
@@ -1,3 +1,5 @@
+var bcrypt = require('bcryptjs');
+
 module.exports = {
   connection: 'localDiskDb',
 
@@ -20,5 +22,31 @@ module.exports = {
       collection: 'basket',
       via: 'user'
     }
+  },
+
+  beforeCreate: function(values, next) {
+    bcrypt.genSalt(10, function(err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(values.password, salt, function(err, hash) {
+        if (err) return next(err);
+
+        values.encryptedPassword = hash;
+        next();
+      });
+    });
+  },
+
+    validPassword: function(password, user, cb) {
+    bcrypt.compare(password, user.encryptedPassword, function(err, match) {
+      if (err) cb(err);
+
+      if (match) {
+        cb(null, true);
+      } else {
+        cb(err);
+      }
+    });
   }
+
 }
