792ec82a4de93c00419bd753e835f8316504831d
rymw3p <kontakt@julian-claus.de>
2018-02-17 10:57:43 +0100

    fix(client): check if dev for service worker

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,4 @@
-if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator && process.env.NODE_ENV !== 'development') {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js')
     .then(registration => console.log('SW registered: ', registration))
