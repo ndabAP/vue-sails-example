04fd37ae7d6d86ec2f84b87777ed95ee8f281770
s8wecy <kontakt@julian-claus.de>
2018-08-13 19:47:11 +0200

    feat(client): add image

--- backend/assets/sw.js
+++ backend/assets/sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.369688827b779a5fb63b75eab48459cd.js", "https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+importScripts("/precache-manifest.54b60bd188314b7a53ca8d52e8ac5dd5.js", "https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 
 /* eslint-disable */
 workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
