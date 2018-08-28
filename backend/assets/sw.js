4e9df59d9d15a8ef88b86623e8537401438f9065
s8wecy <kontakt@julian-claus.de>
2018-08-28 16:00:38 +0200

    build: add production files

--- backend/assets/sw.js
+++ backend/assets/sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.077b3d64f8448b3b0a600c2f5433d56b.js", "https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+importScripts("/precache-manifest.802623bbafa42910de8e114f8f3981a0.js", "https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 
 /* eslint-disable */
 workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
