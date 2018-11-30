f93055c7986d7275974a6821d417085e736ece15
s8wecy <kontakt@julian-claus.de>
2018-11-30 10:21:18 +0100

    chore: extend git commit message rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,25 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'deps',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'security',
+        'style',
+        'test',
+        'wip'
+      ]
+    ]
+  }
+}
