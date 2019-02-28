6ddbcc4b369fa97e71423805280df366c0fad7c0
Julian Claus <kontakt@julian-claus.de>
2019-02-28 06:30:55 +0100

    build: add production files

---
+++ backend/assets/precache-manifest.28eda1f90a81d9bfde2bfaf166d3f21f.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cb72ca9be1e52890315",
+    "url": "/js/0011.d26555d2.js"
+  },
+  {
+    "revision": "1e22d0bbae81de3dc9ce",
+    "url": "/js/83d0.4eb65a7f.js"
+  },
+  {
+    "revision": "43bf89bcde05089277e9",
+    "url": "/js/92d7.1f72dc58.js"
+  },
+  {
+    "revision": "560d20ca9ac5658d16bf",
+    "url": "/js/app.1bcceadd.js"
+  },
+  {
+    "revision": "22b7e24a35f5270dd684",
+    "url": "/css/chunk-01c9.4ddb07be.css"
+  },
+  {
+    "revision": "22b7e24a35f5270dd684",
+    "url": "/js/chunk-01c9.9f7230ba.js"
+  },
+  {
+    "revision": "f98a57d24f3005b06f21",
+    "url": "/css/chunk-04f2.d46b21cd.css"
+  },
+  {
+    "revision": "f98a57d24f3005b06f21",
+    "url": "/js/chunk-04f2.1b246ded.js"
+  },
+  {
+    "revision": "9aa7061667031e9c2dba",
+    "url": "/js/chunk-08b7.8512807a.js"
+  },
+  {
+    "revision": "509b072ffa4180493b53",
+    "url": "/js/chunk-1563.7396ec83.js"
+  },
+  {
+    "revision": "378bbf3854c988d7d08b",
+    "url": "/js/chunk-1a17.30f2d575.js"
+  },
+  {
+    "revision": "7f31c63beba70bf7b8d6",
+    "url": "/js/chunk-3887.aa15bb72.js"
+  },
+  {
+    "revision": "4aedc9b2b7e58d8e743e",
+    "url": "/css/chunk-501f.1eb1bb2f.css"
+  },
+  {
+    "revision": "4aedc9b2b7e58d8e743e",
+    "url": "/js/chunk-501f.e145111d.js"
+  },
+  {
+    "revision": "7c1e76f802589d41cfd5",
+    "url": "/css/chunk-58f5.b1ea0394.css"
+  },
+  {
+    "revision": "7c1e76f802589d41cfd5",
+    "url": "/js/chunk-58f5.62a37769.js"
+  },
+  {
+    "revision": "e9f6f11007417812089e",
+    "url": "/js/chunk-596f.e000c2e0.js"
+  },
+  {
+    "revision": "afe1091aefbf8c1df9d9",
+    "url": "/css/chunk-5e44.062decc4.css"
+  },
+  {
+    "revision": "afe1091aefbf8c1df9d9",
+    "url": "/js/chunk-5e44.8377145e.js"
+  },
+  {
+    "revision": "a875373a8d4bbf4501e2",
+    "url": "/js/chunk-5e5d.1f401c26.js"
+  },
+  {
+    "revision": "0b06c675490ffd7df576",
+    "url": "/js/chunk-6017.a01ecfd3.js"
+  },
+  {
+    "revision": "a1aa58fb0899e13511b1",
+    "url": "/js/chunk-6eaa.c2ea0542.js"
+  },
+  {
+    "revision": "e1d165636ee0de191bc3",
+    "url": "/js/chunk-727a.61973d24.js"
+  },
+  {
+    "revision": "99b7bb7ae474925e9456",
+    "url": "/js/chunk-7644.e3f32036.js"
+  },
+  {
+    "revision": "cebb351cda7861785dc2",
+    "url": "/js/chunk-7bd0.f386b588.js"
+  },
+  {
+    "revision": "435faf69d77a7e1e1478",
+    "url": "/js/chunk-84ba.0c9cad26.js"
+  },
+  {
+    "revision": "6d5ecc03006c032fb0f1",
+    "url": "/js/chunk-a85b.82aa834f.js"
+  },
+  {
+    "revision": "a1510bf2918075a75851",
+    "url": "/js/chunk-abc6.2eb6f8f4.js"
+  },
+  {
+    "revision": "6d05238dd6f7081c6d1c",
+    "url": "/js/chunk-db80.3deb64de.js"
+  },
+  {
+    "revision": "fb929085840cd998dd28",
+    "url": "/js/chunk-vendors.3e9c575e.js"
+  },
+  {
+    "revision": "430f0a872990c82c3ca55b9a44cf46e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2bd7d76fb1ea6f201f8bb1ea1345ba1",
+    "url": "/img/baseline-info-24px.svg"
+  }
+];
--- backend/assets/sw.js
+++ backend/assets/sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.53d6b2f615e35d55accead1fa067cfff.js", "https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+importScripts("/precache-manifest.28eda1f90a81d9bfde2bfaf166d3f21f.js", "https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 
 /* eslint-disable */
 workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
